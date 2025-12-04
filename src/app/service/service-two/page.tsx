276dd1d59afe0697fef49275b069ea622764e6e6
Abhijit47 <akarmakar846@gmail.com>
2025-12-04 22:53:32 +0530

    Refactor service components: remove unused imports and commented-out code, improve code readability and maintainability

--- src/app/service/service-two/page.tsx
+++ src/app/service/service-two/page.tsx
@@ -1,28 +1,32 @@
-import TopNavOne from "@/components/Header/TopNav/TopNavOne"
-import MenuOne from "@/components/Header/Menu/MenuOne"
-import BreadcrumbItem from "@/components/Breadcrumb/BreadcrumbItem"
-import ServiceFilter from "@/components/Section/Service/ServiceFilter"
-import serviceData from '@/data/service.json'
-import CtaOne from "@/components/Section/CTA/CtaOne"
-import Footer from "@/components/Footer/Footer"
+import BreadcrumbItem from '@/components/Breadcrumb/BreadcrumbItem';
+import MenuOne from '@/components/Header/Menu/MenuOne';
+import TopNavOne from '@/components/Header/TopNav/TopNavOne';
+// import ServiceFilter from "@/components/Section/Service/ServiceFilter"
+import Footer from '@/components/Footer/Footer';
+import CtaOne from '@/components/Section/CTA/CtaOne';
 
 export default function ServiceStyleTwo() {
-    return (
-        <>
-            <div className="overflow-x-hidden">
-                <header id="header">
-                    <TopNavOne />
-                    <MenuOne />
-                </header>
-                <main className="content">
-                    <BreadcrumbItem link="Our Services" img="/images/banner/about1.png" title="Our Services" desc="Expand your knowledge and skills in cryptocurrency trading through our educational resources." />
-                    <ServiceFilter data={serviceData} />
-                    <CtaOne />
-                </main>
-                <footer id="footer">
-                    <Footer />
-                </footer>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className='overflow-x-hidden'>
+        <header id='header'>
+          <TopNavOne />
+          <MenuOne />
+        </header>
+        <main className='content'>
+          <BreadcrumbItem
+            link='Our Services'
+            img='/images/banner/about1.png'
+            title='Our Services'
+            desc='Expand your knowledge and skills in cryptocurrency trading through our educational resources.'
+          />
+          {/* <ServiceFilter data={serviceData} /> */}
+          <CtaOne />
+        </main>
+        <footer id='footer'>
+          <Footer />
+        </footer>
+      </div>
+    </>
+  );
 }
